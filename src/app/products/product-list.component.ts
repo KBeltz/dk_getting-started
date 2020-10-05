3255deac48f25e2440de409a15544e5020440856
Kristin (Kikki) Beltz <kdb05506@gmail.com>
2020-10-04 21:09:11 -0500

    Finish module 6

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,6 +5,10 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = "Product List";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = "cart";
   products: any[] = [
     {
       productId: 1,
@@ -57,4 +61,8 @@ export class ProductListComponent {
       imageUrl: "assets/images/xbox-controller.png"
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
