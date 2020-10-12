f8fd7beb8fba09ed3ea7298b92053171a371a8ed
KBeltz <beltz08@me.com>
2020-10-11 22:02:25 -0500

    Add product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from "@angular/common/http"
 
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
+import { ProductDetailComponent } from "./products/product-detail.component";
 import { FormsModule } from "@angular/forms";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
